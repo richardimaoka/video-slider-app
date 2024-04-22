e65283c143023955e18ec0bb098654c7db64b21b
Richard Imaoka <richard.e.imaoka@gmail.com>
2024-04-23 08:42:27 +0900

    add images

--- next/app/page.tsx
+++ next/app/page.tsx
@@ -1,3 +1,18 @@
+import Image from "next/image";
+
 export default function Home() {
-  return <main></main>;
+  const arr = Array.from({ length: 100 }, (_, i) => i + 1);
+  return (
+    <main>
+      {arr.map((n) => (
+        <Image
+          key={n}
+          src={`/images/background (${n}).png`}
+          width={960}
+          height={540}
+          alt={`background ${n}`}
+        />
+      ))}
+    </main>
+  );
 }
