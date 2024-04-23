c6f5a166177289b3cf772bead04f762408c27d9e
Richard Imaoka <richard.e.imaoka@gmail.com>
2024-04-24 08:29:45 +0900

    slider

--- next/app/page.tsx
+++ next/app/page.tsx
@@ -1,18 +1,23 @@
 import Image from "next/image";
+import styles from "./page.module.css";
 
 export default function Home() {
-  const arr = Array.from({ length: 100 }, (_, i) => i + 1);
+  const arr = Array.from({ length: 10 }, (_, i) => i + 1);
   return (
     <main>
-      {arr.map((n) => (
-        <Image
-          key={n}
-          src={`/images/background (${n}).png`}
-          width={960}
-          height={540}
-          alt={`background ${n}`}
-        />
-      ))}
+      <div className={styles.peep}>
+        <div className={styles.slider}>
+          {arr.map((n) => (
+            <Image
+              key={n}
+              src={`/images/background (${n}).png`}
+              width={960}
+              height={540}
+              alt={`background ${n}`}
+            />
+          ))}
+        </div>
+      </div>
     </main>
   );
 }
